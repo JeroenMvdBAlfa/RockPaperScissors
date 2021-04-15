496b5fd925d409fce5623a5d35a64fbd85d9dad0
jeroen <jc.maassenvandenbrink@student.alfa-college.nl>
2021-04-15 18:36:25 +0200

    Create Basic Index header and footer with an image folder and stylesheet

---
+++ header.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Rock Paper Scissors</title>
+    <link href="stylesheet.css" rel="stylesheet" type="text/css" />
+    <link rel="icon" href="img/Logo.png" />
+</head>
+<body>
+    <h1>Rock Paper Scissors</h1>
